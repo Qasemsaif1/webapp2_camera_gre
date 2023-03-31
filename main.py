68a9f3b4bbd6bce115d60f0a30ded6e7af063ed9
Qasem <qasemsaiff@gmail.com>
2023-03-31 23:47:13 +0200

    Fixing the if method and add the expander.

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import streamlit as st
 from PIL import Image as ig
 
+
 # Starting the App
 st.header("Camera app")
 
 # Taking a Picture
 
 # with st.expander(label="") Thelines bilow will be indented
-button = st.button ("Open Camera")
-if button:
+with st.expander("Take a picture"):
     photo = st.camera_input("")
 
-    if photo:
-        # Create a pillow image instant
-        image = ig.open (photo)
-        # convert the image grey scale
-        gre_image = image.convert("L")
-        # Show the image
-        st.image(gre_image)
+if photo:
+    # Create a pillow image instant
+    image = ig.open (photo)
+    # convert the image grey scale
+    gre_image = image.convert("L")
+    # Show the image
+    st.image(gre_image)
 
 
 
