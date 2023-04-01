346462160a7e975a6e8f611921949bbce71ce014
Qasem <qasemsaiff@gmail.com>
2023-04-01 15:16:57 +0200

    organize the lines

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import functions as fc
 
 # Starting the App
 st.header("Camera app")
-# Taking a Picture
 
-# with st.expander(label="") The lines below will be indented
+# Taking a Picture
 with st.expander("Take a picture"):
     photo = st.camera_input("")
 
 uploaded_image = st.file_uploader("Upload Image")
 
+# Convert the image
 fc.grey_photos(photo)
 fc.grey_photos(uploaded_image)
